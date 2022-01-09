5f0bb549ed769b6a44c1f1912a1562be7467fffe
Tuyen Nguyen Ngoc <tuyennguyenngoc279@gmail.com>
2022-01-09 15:07:26 +0700

    wip

--- docs/servers.js
+++ docs/servers.js
@@ -1,8 +1,8 @@
 module.exports = {
     servers:[
         {
-            // url:"https://city-swagger.azurewebsites.net",
-            url:"http://localhost:8000",
+            url:"https://city-swagger.azurewebsites.net",
+            // url:"http://localhost:8000",
             description:"Local server"
         },
     ]
